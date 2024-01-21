2094834386abf5b63dd39c654ca594992d18f523
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-01-21 17:12:14 +0700

    Update web_google.py

--- web_google.py
+++ web_google.py
@@ -1,1 +1,64 @@
+import PyQt5
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
 
+class Browser(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("http://www.google.com"))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # Navigation Bar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        # Back Button
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        # Forward Button
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        # Reload Button
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        # Home Button
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        # URL Bar
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        # Updating URL bar
+        self.browser.urlChanged.connect(self.update_urlbar)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("http://www.google.com"))
+
+    def navigate_to_url(self):
+        q = QUrl(self.url_bar.text())
+        if q.scheme() == "":
+            q.setScheme("http")
+
+        self.browser.setUrl(q)
+
+    def update_urlbar(self, q):
+        self.url_bar.setText(q.toString())
+        self.url_bar.setCursorPosition(0)
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName("Simple Browser")
+window = Browser()
+app.exec_()
+      
